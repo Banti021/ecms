f402e5c9652e433ac0f3eb06369d111daf9c80a4
Banti021 <igor.lasota21@gmail.com>
2024-06-12 21:53:15 +0200

    Added fronted router

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,11 +1,8 @@
 <template>
-  <BaseLayout />
+  <div>
+    <router-view />
+  </div>
 </template>
 
 <script setup>
-import BaseLayout from './components/BaseLayout.vue'
 </script>
-
-<style scoped>
-/* Add any styles for App.vue here if needed */
-</style>
--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,17 +1,29 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import { createRouter, createWebHistory } from 'vue-router';
+import HomeView from '../views/HomeView.vue';
+import ReservationView from '../views/ReservationView.vue';
+import FacilityAreasView from '../views/FacilityAreasView.vue';
 
 const routes = [
     {
         path: '/',
         name: 'Home',
-        component: HomeView
+        component: HomeView,
     },
-]
+    {
+        path: '/reserve-area',
+        name: 'ReserveArea',
+        component: ReservationView,
+    },
+    {
+        path: '/reserve-area/:id',
+        name: 'FacilityAreas',
+        component: FacilityAreasView,
+    },
+];
 
 const router = createRouter({
     history: createWebHistory(process.env.BASE_URL),
-    routes
-})
+    routes,
+});
 
-export default router
+export default router;
