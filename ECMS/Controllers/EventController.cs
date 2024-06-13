028cee1bf20983810ca98fd68ac31f2363c8c2eb
Banti021 <igor.lasota21@gmail.com>
2024-06-13 19:03:32 +0200

    EventController dependency injection

--- ECMS/Controllers/EventController.cs
+++ ECMS/Controllers/EventController.cs
@@ -7,54 +7,47 @@ namespace ECMS.Controllers
 {
     [ApiController]
     [Route("api/[controller]")]
-    public class EventController : ControllerBase
+    public class EventController(EventService service) : ControllerBase
     {
-        private readonly EventService _service;
-
-        public EventController(EventService service)
-        {
-            _service = service;
-        }
-
         [HttpGet]
         public async Task<IActionResult> GetEvents(CancellationToken token)
         {
-            var events = await _service.GetAllEventsAsync(token);
+            var events = await service.GetAllEventsAsync(token);
             return Ok(events);
         }
 
         [HttpGet("{id}")]
         public async Task<IActionResult> GetEvent(int id, CancellationToken token)
         {
-            var eventItem = await _service.GetEventByIdAsync(id, token);
+            var eventItem = await service.GetEventByIdAsync(id, token);
             return Ok(eventItem);
         }
 
         [HttpPost]
         public async Task<IActionResult> AddEvent([FromBody] EventDto eventDto)
         {
-            var eventItem = await _service.AddEventAsync(eventDto);
+            var eventItem = await service.AddEventAsync(eventDto);
             return Ok(eventItem);
         }
 
         [HttpPut("{id}")]
         public async Task<IActionResult> UpdateEvent(int id, [FromBody] EventDto eventDto)
         {
-            var eventItem = await _service.UpdateEventAsync(id, eventDto);
+            var eventItem = await service.UpdateEventAsync(id, eventDto);
             return Ok(eventItem);
         }
 
         [HttpDelete("{id}")]
         public async Task<IActionResult> DeleteEvent(int id)
         {
-            await _service.DeleteEventAsync(id);
+            await service.DeleteEventAsync(id);
             return NoContent();
         }
         
         [HttpPost("register")]
         public async Task<IActionResult> RegisterForEvent([FromBody] EventRegistrationDto eventRegistrationDto)
         {
-            await _service.RegisterForEventAsync(eventRegistrationDto);
+            await service.RegisterForEventAsync(eventRegistrationDto);
             return Ok("Registration successful");
         }
         
@@ -62,7 +55,7 @@ namespace ECMS.Controllers
         [HttpGet("category/{category}")]
         public async Task<IActionResult> GetEventsByCategory(EventCategory category, CancellationToken token)
         {
-            var events = await _service.GetEventsByCategoryAsync(category, token);
+            var events = await service.GetEventsByCategoryAsync(category, token);
             return Ok(events);
         }
     }
