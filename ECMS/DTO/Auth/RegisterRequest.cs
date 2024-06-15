eb913dfcd6312b442449fb120ae1ab2921a3ad2f
Banti021 <igor.lasota21@gmail.com>
2024-06-15 18:09:43 +0200

    Added login request dto

---
+++ ECMS/DTO/Auth/RegisterRequest.cs
@@ -0,0 +1,14 @@
+namespace ECMS.DTO.Auth
+{
+    public class RegisterRequest
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string UserName { get; set; }
+        public string Password { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Email { get; set; }
+        public string Role { get; set; }
+        public DateTime DateOfBirth { get; set; }
+    }
+}
